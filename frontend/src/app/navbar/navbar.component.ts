e6e15174a4442e6fce6ee9be517d4ed995a0f259
aarushi04 <49411663+aarushi04@users.noreply.github.com>
2020-11-01 19:28:25 +0530

    Add files via upload

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
+  title = 'Pantry Pal';
+
+  ngOnInit() {
+  }
+
+  handleLogout() {
+    this.authService.logout();
+  }
+
+  openSignupModal() {
+    document.getElementsByClassName('signup-modal')[0].classList.add('md-show');
+  }
+
+  openLoginModal() {
+    document.getElementsByClassName('login-modal')[0].classList.add('md-show');
+  }
+
+}
